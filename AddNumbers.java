f3c01c61fd9b76fd44f7151b9bec0b441c272f39
Asmi Gawade <109681992+asmigawade@users.noreply.github.com>
2023-11-01 21:32:12 +0530

    Add files via upload

---
+++ AddNumbers.java
@@ -0,0 +1,13 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        // Define two numbers to add
+        int number1 = 5;
+        int number2 = 7;
+        
+        // Calculate the sum
+        int sum = number1 + number2;
+        
+        // Print the result
+        System.out.println("The sum of " + number1 + " and " + number2 + " is: " + sum);
+    }
+}
